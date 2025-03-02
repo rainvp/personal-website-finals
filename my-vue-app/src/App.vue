cbc580a75af68a8da4bcdd6dc96db8de626b37ce
Rain Cel-Anne Sophia V. Presa <rvpresa@student.apc.edu.ph>
2025-03-02 04:25:43 +0000

    test huhu

--- my-vue-app/src/App.vue
+++ my-vue-app/src/App.vue
@@ -1,16 +1,10 @@
 <template>
   <div>
-    <NavbarBanner />
-    <Guestbook/>
     <router-view />
-    <Footer />  <!-- Footer is used here -->
     
     
   </div>
 </template>
 
 <script setup>
-import NavbarBanner from './components/NavbarBanner.vue';
-import Guestbook from './components/Guestbook.vue';
-import Footer from './components/Footer.vue'; 
 </script>
