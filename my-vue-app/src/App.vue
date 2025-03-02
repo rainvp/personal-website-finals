871f8effe2a0a150f00a69fd99db2d47f4c30d08
Rain Cel-Anne Sophia V. Presa <rvpresa@student.apc.edu.ph>
2025-03-02 04:27:22 +0000

    huhuhuhu almost

--- my-vue-app/src/App.vue
+++ my-vue-app/src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <div>
     <router-view />
+    <Footer />  <!-- Footer is used here -->
     
     
   </div>
 </template>
 
 <script setup>
+import Footer from './components/Footer.vue'; 
 </script>
